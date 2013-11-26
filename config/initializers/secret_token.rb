1f845d03569f080c266890e111d0b8f17ff10fc3
Abdi Ali <abdirazak.ali.12@live.com>
2013-11-26 17:32:17 +0000

    intial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Imagesaber::Application.config.secret_key_base = 'ab757919fd7b534374008c0b5044fefdd0af4672f49eb09a3e9e276d21b5350e60d7ac4190d703279f2b66b8867e21546eee6a26f3f7b3707dc99473e228f7bc'
